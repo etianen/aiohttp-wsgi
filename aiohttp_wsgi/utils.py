8413fe6eae59f7902c22def9f1247d0aef0d9468
Dave Hall <dave@etianen.com>
2015-03-09 16:34:14 +0000

    Fixing sockname parsing for linux

--- aiohttp_wsgi/utils.py
+++ aiohttp_wsgi/utils.py
@@ -1,4 +1,4 @@
 def parse_sockname(sockname):
     if isinstance(sockname, tuple):
         return str(sockname[0]), str(sockname[1])
-    return "unix", str(sockname)
+    return "unix", str(sockname, "latin1") if isinstance(sockname, (bytes, bytearray, memoryview)) else str(sockname)
