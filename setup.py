2f36d970681c9b74a51bce17e23a6e624cad313f
Dave Hall <dave@etianen.com>
2015-05-15 18:16:13 +0100

    Bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "aiohttp-wsgi",
-    version = "0.2.0",
+    version = "0.2.1",
     license = "BSD",
     description = "WSGI adapter for aiohttp.",
     author = "Dave Hall",
