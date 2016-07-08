d7a8c564bd6c3194e606626e93f178095d2168ea
Dave Hall <dave@etianen.com>
2016-07-08 17:20:07 +0100

    Removing aiohttp dependency on setup.py

--- aiohttp_wsgi/__init__.py
+++ aiohttp_wsgi/__init__.py
@@ -2,8 +2,15 @@
 WSGI adapter for aiohttp.
 """
 
-from aiohttp_wsgi.api import configure_server, close_server, serve  # noqa
-from aiohttp_wsgi.wsgi import WSGIHandler  # noqa
+try:
+    import aiohttp  # noqa
+except ImportError:
+    # The top-level API requires aiohttp, which might not be present if setup.py
+    # is importing aiohttp_wsgi to get __version__.
+    pass
+else:
+    from aiohttp_wsgi.api import configure_server, close_server, serve  # noqa
+    from aiohttp_wsgi.wsgi import WSGIHandler  # noqa
 
 
 __version__ = (0, 4, 0)
