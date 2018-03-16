1a0b1bd4099e01a882b8a40616b6661c99db0682
Dave Hall <dave@etianen.com>
2018-03-16 11:15:18 +0000

    Losing force_str shim

--- aiohttp_wsgi/utils.py
+++ aiohttp_wsgi/utils.py
@@ -1,8 +1,4 @@
-def force_str(value):
-    return str(value, "latin1") if isinstance(value, (bytes, bytearray, memoryview)) else str(value)
-
-
 def parse_sockname(sockname):
     if isinstance(sockname, tuple):
-        return force_str(sockname[0]), force_str(sockname[1])
-    return "unix", force_str(sockname)
+        return sockname[0], str(sockname[1])
+    return "unix", sockname
