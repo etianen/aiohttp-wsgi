5d86ba26614dbde4de1243b61f5ba05e3890c6a8
Dave Hall <dave@etianen.com>
2018-03-16 11:10:30 +0000

    Upgrading to aiohttp3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/etianen/aiohttp-wsgi",
     packages=find_packages(exclude=("tests",)),
     install_requires=[
-        "aiohttp>=2.3.1,<3",
+        "aiohttp>=3,<4",
     ],
     entry_points={
         "console_scripts": ["aiohttp-wsgi-serve=aiohttp_wsgi.__main__:main"],
