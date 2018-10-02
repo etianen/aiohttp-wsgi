b02869b61c98780af379e086f3d34b73f7cea5a3
Michael Käufl <michael-k@users.noreply.github.com>
2018-10-02 14:20:38 +0200

    Remove Python 3.4 from classifiers

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Framework :: Django",
     ],
